bc9b4881bc6d98c6501f8c0e7712b6aa2b41b509
chris-afans <120418610+chris-afans@users.noreply.github.com>
2023-03-12 17:28:49 -0500

    mousemaze

---
+++ js/mousemaze.js
@@ -0,0 +1,100 @@
+
+$(document).ready(function(){
+    initialize();
+    
+
+    
+    // $(".boundary").mouseout(function(){
+    //    $(".boundary").each(function() {
+    //     $(this).css({
+    //         "background-color" :"#eeeeee",
+    //         "cursor" : "pointer"
+    //     }); 
+    //    }) 
+       
+    // });
+});
+
+let lost = false;
+let start = false;
+let win = false;
+function initialize() {
+    addyouLoseEventListener();
+    addyouWinEventListener();
+    addRestartMazeEventListener();
+    addDisallowCheatingEventListener();
+}
+function addyouLoseEventListener(){
+    var elements = document.getElementsByClassName("boundary");
+    console.log(elements);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('mouseover', youLose)
+      }
+    //document.querySelector("div.boundary").addEventListener('hover', youLose);
+}
+
+function addyouWinEventListener() {
+    document.querySelector("#end").addEventListener('mouseover', youWin);
+}
+
+function addRestartMazeEventListener() {
+    document.querySelector("#start").addEventListener('click', restartMaze);
+}
+
+function addDisallowCheatingEventListener() {
+    // var elements = document.querySelectorAll("p, h1, h2");
+    // console.log(elements);
+    // for (var i = 0; i < elements.length; i++) {
+        
+    //     elements[i].addEventListener('mouseover', disallowCheating)
+        
+        
+    //   
+    document.querySelector("#maze").addEventListener('mouseleave', disallowCheating);
+    
+}
+
+function disallowCheating() {
+    if(start == true){
+        youLose();
+    }
+}
+
+function youLose() {
+      lost = true;
+      if(win == false){  
+      $("div.boundary").css({
+             "background-color" :"red",
+             "cursor" : "pointer"
+         });
+         
+        $('#status').html("You Lose!");
+        
+        }
+}
+
+function youWin() {
+    win = true;
+    if(lost == false){
+        $('#status').html("You Win!"); 
+    }
+}
+
+function restartMaze() {
+    
+    if($(".boundary").each(function(){
+        $(this).css({
+            "background-color" : "red"
+        });
+    })){
+        $(".boundary").each(function() {
+            $(this).css({
+                "background-color" :"#eeeeee"                
+            });
+           });
+           $("#status").html("Click the &quot;S&quot; to begin.");
+           lost = false; 
+           win = false;
+           start = true;
+    }
+}
